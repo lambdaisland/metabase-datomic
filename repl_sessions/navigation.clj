f24632f2705a183267e7c080237bfd7f953742cb
Arne Brasseur <arne@arnebrasseur.net>
2019-05-23 13:56:41 +0200

    Update ADRs

---
+++ repl_sessions/navigation.clj
@@ -0,0 +1,67 @@
+(ns navigation
+  (:require [datomic.api :as d]))
+
+{:rules
+ [[(sub-accounts ?p ?c)
+   [?p :account/children ?c]]
+  [(sub-accounts ?p ?d)
+   [?p :account/children ?c]
+   (sub-accounts ?c ?d)]
+  ]
+
+ :relationships
+ {:account
+  {:journal-entry-lines
+   {:path [:journal-entry-line/_account]
+    :target :journal-entry-line}
+
+   :subaccounts
+   {:path [sub-accounts]
+    :target :account}
+
+   :parent-accounts
+   {:path [_sub-accounts]
+    :target :account}
+   }
+
+  :journal-entry-line
+  {:fiscal-year
+   {:path [:journal-entry/_journal-entry-lines
+           :ledger/_journal-entries
+           :fiscal-year/_ledgers]
+    :target :fiscal-year}}
+
+  }}
+
+(defn path-binding [from-sym to-sym path]
+  (let [segment-binding
+        (fn [from to seg]
+          (cond
+            (keyword? seg)
+            (if (= \_ (first (name seg)))
+              [to (keyword (namespace seg) (subs (name seg) 1)) from]
+              [from seg to])
+
+            (symbol? seg)
+            (if (= \_ (first (name seg)))
+              (list (symbol (subs (str seg) 1)) to from)
+              (list seg from to))))]
+    (loop [[p & path] path
+           binding    []
+           from-sym   from-sym]
+      (if (seq path)
+        (let [next-from (gensym "?")]
+          (recur path
+                 (conj binding (segment-binding from-sym next-from p))
+                 next-from))
+        (conj binding (segment-binding from-sym to-sym p))))))
+
+(path-binding '?jel '?fj '[:journal-entry/_journal-entry-lines
+                           :ledger/_journal-entries
+                           :fiscal-year/_ledgers])
+
+
+(d/q '{:find [?e]
+       :where [[?e :db/ident]]} (db))
+
+(user/refer-repl)
